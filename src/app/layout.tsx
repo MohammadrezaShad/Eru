8dc230b0c923a34dbc3c5a4e3abb3d0a5e607578
shadmobaraki <shadmobaraki@plaza.ir>
2024-05-23 12:59:41 +0330

    feat : add next theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="fa" dir="rtl">
+    <html lang="fa" dir="rtl" suppressHydrationWarning>
       <body className={vazirmatn.className}>
         <Providers>
           <main>
--- src/providers/main.provider.tsx
+++ src/providers/main.provider.tsx
@@ -1,12 +1,16 @@
 'use client';
 
 import React from 'react';
-import { QueryClientProvider } from '@/providers';
+import { QueryClientProvider, ThemeProvider } from '@/providers';
 
 type MainProvidersProps = {
   children: React.ReactNode;
 };
 
 export default function MainProviders({ children }: MainProvidersProps) {
-  return <QueryClientProvider>{children}</QueryClientProvider>;
+  return (
+    <ThemeProvider>
+      <QueryClientProvider>{children}</QueryClientProvider>;
+    </ThemeProvider>
+  );
 }
