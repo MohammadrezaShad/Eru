aab67caf7d7b456d4b29f44320a5eebf7ec4a0c5
shadmobaraki <shadmobaraki@plaza.ir>
2024-05-25 12:59:38 +0330

    feat : add svgr

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    reactCompiler: true,
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          // options: {
+          //   svgo: false, // Disable SVGO if you need to
+          // },
+        },
+      ],
+    });
+
+    return config;
+  },
+};
 
 export default nextConfig;
